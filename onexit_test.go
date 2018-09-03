70b496cf950d59a464e71f1b3c0c5e2f3ee90ae6
dc0d <kaveh.shahbazian@gmail.com>
2018-09-04 00:52:48 +0430

    test deferred

--- onexit_test.go
+++ onexit_test.go
@@ -6,6 +6,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestDeferred(t *testing.T) {
+	assert := assert.New(t)
+	d := newDeferred()
+
+	var order []int
+
+	// the functions are being registered from lowest priority
+	// to highest.
+	for i := -10; i <= 10; i++ {
+		i := i
+		d.Register(func() {
+			order = append(order, i)
+		}, i)
+	}
+
+	go d.Cleanup()
+	go d.Cleanup()
+	go d.Cleanup()
+
+	<-d.Done()
+
+	// the highest priority will run first.
+	for i := 0; i < 21; i++ {
+		assert.Equal(20-i-10, order[i])
+	}
+}
+
 func TestRegister(t *testing.T) {
 	assert := assert.New(t)
 
